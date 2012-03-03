b10803aef9a79cb55c035947a9f6905bc0e8c2ab
Vladimir Dronnikov <dronnikov@gmail.com>
2012-03-03 01:41:13 -0500

    test and readme updated

--- test.lua
+++ test.lua
@@ -1,16 +1,29 @@
 #!/usr/bin/env luvit
 
 local Stack = require('stack')
-local function handler(req, res)
+
+local function make_handler()
   return Stack.stack(
     require('static')('/', {
-      directory = __dirname,   -- root of static server
-      max_age = 24*60*60*1000, -- cache for 1 day
-      is_cacheable = function (file)
+      -- root of static server
+      directory = __dirname,
+      -- cache in the browser for 1 day
+      max_age = 24*60*60*1000,
+      -- cache served files
+      is_cacheable = function (file_stat)
+        -- cache only small files
+        -- return file_stat.size < 65536
+        -- cache all
         return true
-      end,                     -- can cache served files
+      end,
+      -- follow symlinks
+      follow = true,
+      -- redirect directories to underneath index.html
+      auto_index = 'README.md',
     })
   )
 end
-require('http').createServer(handler()):listen(8080, '0.0.0.0')
+
+require('http').createServer(make_handler()):listen(8080, '0.0.0.0')
+
 print('Static file server listening at http://localhost:8080/')
